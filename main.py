a99e713b8cf275527ac2a463bb8fa7d3d2857dc3
magantisyamala <72926704+magantisyamala@users.noreply.github.com>
2022-09-08 14:27:11 +0530

    added one print statement newly

--- main.py
+++ main.py
@@ -15,4 +15,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print("How are you")
     print(add(2,3))
+    print("welcome to github")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
